cfe309cefaee9e6adbf37976f650b2c109467130
yesisbore <60872762+yesisbore@users.noreply.github.com>
2022-05-23 01:02:34 +0900

    init
    init

---
+++ C#/Example01/Example01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace Example01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Cv2.GetVersionString());
+
+            // 이미지 크기 표현 
+            //Mat color = new Mat(new Size(width,height),MatType.cv_8uc3);
+            //Mat gray = new Mat(rows,cols,MatType.cv_8uc1);
+        }
+    }
+}
---
+++ pythonProject/main.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+
+# print(cv2.__version__)
+
+# olor = np.zeros((height,width,3),np.unit8)
+# gray = np.zeros((rows,cols,1),np.unit8)
